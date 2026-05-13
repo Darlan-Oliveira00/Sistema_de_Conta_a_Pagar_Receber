b3b6aa0be85b8753593b2f084fee8d2291d1dc23
Henrique C. Rodrigues <henriquehoor18@hotmail.com>
2026-05-12 21:49:31 -0300

    fix: correção na rota cliente e mudança na criação da tabela cliente e fornecedor no campo data

--- backend/API/validações.py
+++ backend/API/validações.py
@@ -24,15 +24,12 @@ def validacao_cpf(cpf, nome_completo, data_nascimento):
         return response.status_code
     else:
         resultado = response.json()
-        pprint(resultado)
         resultado_data_nascimento = datetime.strptime(resultado['data']['birthDate'], '%d/%m/%Y')
         data_nascimento = datetime.strptime(data_nascimento, '%Y-%m-%d')
         resultado_nome = ''.join(resultado['data']['nameUpper'].replace(' ', ''))
-
         if resultado_nome == ''.join(nome_completo.replace(' ', '')).upper() and data_nascimento == resultado_data_nascimento:
-            return True
-        return False
-
+            return 200
+        return 400
 
 
 def validacao_cnpj(cnpj,nome_oficial_empresa ):
@@ -48,6 +45,7 @@ def validacao_cnpj(cnpj,nome_oficial_empresa ):
         resultado = response.json()
         nome_oficial_empresa = ''.join(re.sub(r'\W', '', nome_oficial_empresa)).upper()
         nome_oficial_empresa = remover_acentos(nome_oficial_empresa)
+        print(nome_oficial_empresa, resultado['razao_social'])
         if nome_oficial_empresa == ''.join(re.sub(r'\W', '', resultado['razao_social']).upper()):
             return {'cnpj' : resultado['cnpj'],
                     'nome_oficial_empresa': resultado['razao_social'],
@@ -70,6 +68,6 @@ def remover_acentos(texto):
 
 
 if __name__ == '__main__':
-    #resposta = validacao_cnpj('60.701.190/0001-04', 'Itaú Unibanco S.A.  ')
-    resposta = validacao_cpf('06331229205', '  henrique costa rodrigues   ', '2004-09-13')
+    resposta = validacao_cnpj('34.028.316/0001-03', 'Empresa Brasileira de Correios e Telégrafos ')
+    #resposta = validacao_cpf('06331229205', '  henrique costa rodrigues   ', '2004-09-13')
     print(resposta)
